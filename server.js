ce0780f5831aa45b1ea8580a744b384b6d4ffca9
Facundo Irazusta <facundoirazusta@gmail.com>
2022-12-29 15:39:43 -0300

    Initial setup

---
+++ server.js
@@ -0,0 +1,28 @@
+require("dotenv").config();
+
+const express = require("express");
+const routes = require("./routes/routes");
+const dbInitialSetup = require("./dbInitialSetup");
+const APP_PORT = process.env.APP_PORT || 3000;
+const app = express();
+
+console.log(`
+=================================================================
+=================================================================
+=================================================================
+=================================================================
+=================================================================
+`);
+
+app.use(express.static("public"));
+app.use(express.urlencoded({ extended: true }));
+app.set("view engine", "ejs");
+
+routes(app);
+
+// dbInitialSetup();
+
+app.listen(APP_PORT, () => {
+  console.log(`\n[Express] Servidor corriendo en el puerto ${APP_PORT}.`);
+  console.log(`[Express] Ingresar a http://localhost:${APP_PORT}.\n`);
+});
