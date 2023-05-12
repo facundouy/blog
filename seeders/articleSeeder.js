0f64316626705bed3af17dcaa3e0df8757906081
Facundo Irazusta <facundoirazusta@gmail.com>
2023-05-12 17:57:57 -0300

    Dependencies updated

--- seeders/articleSeeder.js
+++ seeders/articleSeeder.js
@@ -2,21 +2,22 @@ const { faker } = require("@faker-js/faker");
 const { Article } = require("../models/models");
 const titles = require("./resources/titles");
 
+function contentCreator() {
+  const numberOfParagraphs = Math.floor(4 * Math.random() + 3);
+  let articleContent = "";
+  for (let i = 0; i < numberOfParagraphs; i++) {
+    // TODO: add an image to each article
+    articleContent += "<p>";
+    articleContent += faker.lorem.sentences(10);
+    articleContent += "</p>";
+  }
+  return articleContent;
+}
+
 module.exports = async () => {
   const articles = [];
 
   for (let i = 0; i < 30; i++) {
-    function contentCreator() {
-      const numberOfParagraphs = Math.floor(4 * Math.random() + 3);
-      let articleContent = "";
-      for (let i = 0; i < numberOfParagraphs; i++) {
-        // TODO: add an image to each article
-        articleContent += "<p>";
-        articleContent += faker.lorem.sentences(10);
-        articleContent += "</p>";
-      }
-      return articleContent;
-    }
     articles.push({
       title: titles[i],
       content: contentCreator(),
